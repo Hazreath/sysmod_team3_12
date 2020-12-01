715f5a6eb317903dace482f1a94f0c6f2b51c978
Maxime Moreau <contact@maxime-moreau.fr>
2020-12-01 18:36:39 +0200

    fix cors

--- main.py
+++ main.py
@@ -7,21 +7,15 @@ from sql_app.database import engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins="*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.add_middleware(CORSMiddleware, allow_origins=['*'])
 """
 Import routes
 """
