061dc67ee187f894c49084df29795e8a35a16d27
Hazreath <benjamincamp.it@gmail.com>
2020-12-02 15:23:06 +0200

    before modify transaction

--- web/res/js/login.js
+++ web/res/js/login.js
@@ -2,45 +2,22 @@ $(document).ready(function() {
     console.log("ready")
     
     // TODO REMOVE ONLY FOR TEST
-    document.getElementById("username").value = "benji@aled.fr"
-    document.getElementById("password").value = "aled"
+    document.getElementById("username").value = "a@b.fr"
+    document.getElementById("password").value = "zzz"
     
     $("#login-button").click(function (e) {
           e.preventDefault();
          // Get username & password
          let u = document.getElementById("username").value
          let p = document.getElementById("password").value
-         if (u && p) {
-            login(u,p)
+         
+         if (!u || !p) {
+            errorToast("One of the required field is empty !")
+         } else if (!isValidEmail(u)) {
+             errorToast("Invalid email format !")
          } else {
-            // TODO Display error message
-            alert("One of the field was left empty !")
+            login(u,p)
          }
 
     })
 })
-var URL_WS_TOKEN = "http://localhost:8000/token"
-function login(username, password) {
-    var url_auth = URL_WS_TOKEN + "?username=" + username + "&password="
-        + password
-    $.ajax({
-        url : URL_WS_TOKEN,
-        type:"POST",
-//      dataType:'jsonp',
-//        crossDomain: true,
-//        type:'json',
-        headers: {  
-            'Access-Control-Allow-Origin': 'x-requested-with',
-            'Content-Type':'application/json'
-        },
-
-        data: {
-            username:username,
-            password:password
-        },
-        success: function(json) {
-            console.log(json)
-            // Keep token in memory
-        }
-    })
-}
